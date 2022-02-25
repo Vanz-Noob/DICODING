c454c5b5241723bee9bbdd7b54e370f2b05977f2
Vanz-Noob <renaldivanza@gmail.com>
2022-02-25 17:13:21 +0700

    feat: add last quiz

--- JavaScript/Learning/tets.js
+++ JavaScript/Learning/tets.js
@@ -1,31 +1,7 @@
-class NetworkError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'NetworkError';
-    }
-}
-
-const fetchingUserFromInternet = (isOffline) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (!isOffline) {
-                resolve({ name: 'John', age: 18 });
-            } else {
-                reject(new NetworkError('Gagal mendapatkan data dari internet'));
-            }
-        }, 500);
-    });
-};
-
-
-async function gettingUserName() {
-    try {
-        const fetchUser = await fetchingUserFromInternet(false);
-        return fetchUser.name;
-    } catch (NetworkError) {
-        return NetworkError;
-    }
-};
-
-gettingUserName();
-// console.log(gettingUserName());
+test('should return the max number between 3 arguments', () => {
+    expect(findMax(1, 2, 3)).toEqual(3);
+    expect(findMax(5, 1, 2)).toEqual(5);
+    expect(findMax(102, 404, 48)).toEqual(404);
+    expect(findMax(-1, 0, -1)).toEqual(0);
+    expect(findMax(2, 2, 2)).toEqual(2);
+})
